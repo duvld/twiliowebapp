998a7e6f1c5c9a28d4743e90b96cf392febf8657
duvld <ollejna@gmail.com>
2019-02-14 22:42:24 -0500

    Updates

---
+++ precache-manifest.40f0ab46894dd8b8066eefc9c6c1304f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fresh/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7dc8f9623f100de0d04e",
+    "url": "/fresh/static/js/runtime~main.7dc8f962.js"
+  },
+  {
+    "revision": "6126548c7a32659f71cf",
+    "url": "/fresh/static/js/main.6126548c.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/fresh/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "6126548c7a32659f71cf",
+    "url": "/fresh/static/css/main.f65c3095.chunk.css"
+  },
+  {
+    "revision": "814968933ea53f1829a7062757226a28",
+    "url": "/fresh/index.html"
+  }
+];
