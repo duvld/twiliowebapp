65d2673bfa98a71cfb9a43f65609da297a6aee16
duvld <ollejna@gmail.com>
2019-02-20 17:31:17 -0500

    Updates

---
+++ precache-manifest.bc8481a2ef87b8259839c65d2b1cdf38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffc6907dcb9328e94715",
+    "url": "/twiliowebapp/static/js/runtime~main.ffc6907d.js"
+  },
+  {
+    "revision": "10be7475e9c6006a0775",
+    "url": "/twiliowebapp/static/js/main.10be7475.chunk.js"
+  },
+  {
+    "revision": "02b0e53b9684d455ee82",
+    "url": "/twiliowebapp/static/js/2.02b0e53b.chunk.js"
+  },
+  {
+    "revision": "10be7475e9c6006a0775",
+    "url": "/twiliowebapp/static/css/main.1a9122e3.chunk.css"
+  },
+  {
+    "revision": "35251ed6de547af396eaea5c8372cfa2",
+    "url": "/twiliowebapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/twiliowebapp/precache-manifest.bc8481a2ef87b8259839c65d2b1cdf38.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/twiliowebapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
